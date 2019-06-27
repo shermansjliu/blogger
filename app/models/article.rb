45335a725a44026786d5f0c513a2e9ab77897f4b
Sherman Liu <shermanliu03@gmail.com>
2019-06-27 23:35:57 +0800

    fix delete article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-    has_many :comments
-    has_many :taggings
+    has_many :comments, dependent: :delete_all
+    has_many :taggings, dependent: :delete_all
     has_many :tags, through: :taggings
     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 
